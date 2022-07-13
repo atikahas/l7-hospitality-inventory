459b58c4f7ffbfb9ef091599a76e5b5482824aaf
atikahas <45572226+atikahas@users.noreply.github.com>
2022-07-13 12:10:30 +0800

    update

--- database/migrations/2022_07_13_103259_create_item_purchasehousekeeping_history_table.php
+++ database/migrations/2022_07_13_103259_create_item_purchasehousekeeping_history_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateItemBuyinghousekeepingHistoryTable extends Migration
+class CreateItemPurchasehousekeepingHistoryTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,14 +13,14 @@ class CreateItemBuyinghousekeepingHistoryTable extends Migration
      */
     public function up()
     {
-        Schema::create('item_buyinghousekeeping_history', function (Blueprint $table) {
+        Schema::create('item_purchasehousekeeping_history', function (Blueprint $table) {
             $table->id();
             $table->integer('item_houskeeping_id')->nullable();
             $table->string('detail')->nullable();
             $table->string('shopname')->nullable();
             $table->string('image');
-            $table->integer('buying_price')->nullable();
-            $table->date('buying_date')->nullable();
+            $table->integer('purchase_price')->nullable();
+            $table->date('purchase_date')->nullable();
             $table->integer('quantity')->nullable();
             $table->timestamps();
             $table->softDeletes();
@@ -34,6 +34,6 @@ class CreateItemBuyinghousekeepingHistoryTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('item_buyinghousekeeping_history');
+        Schema::dropIfExists('item_purchasehousekeeping_history');
     }
 }
