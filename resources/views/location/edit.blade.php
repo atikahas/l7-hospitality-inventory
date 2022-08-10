9aaaffbbcd059d7bb65d32ea69bfa417c7c24b60
atikahas <45572226+atikahas@users.noreply.github.com>
2022-08-10 09:58:50 +0800

    update edit layout

--- resources/views/location/edit.blade.php
+++ resources/views/location/edit.blade.php
@@ -1,44 +1,36 @@
-@extends('layouts.default')
+@extends('layouts.skydash')
 @section('title', 'Location')
 @section('location', 'active')
 @section('content')
 
-<div class="container-fluid dashboard">
-    <div class="content-header">
-        <h1>Location</h1>
-        <p></p>
-    </div>
-    <div class="row">
-
-        <div class="col-md-12">
-            <div class="card">
-                <div class="card-header">
-                    <h4>Edit Location</h4>
-                </div>
-                <form action="" method="post">
-                @csrf
-                    <div class="card-body"> 
-                        <div class="mb-3">
-                            <label>Location</label>
-                            <input type="text" class="form-control" name="name" value="{{$location->name}}" required>
-                        </div>
-                    </div>
-                    <div class="card-footer text-right">
-                        <button class="btn btn-primary">Save</button>
-                    </div>
-                </form>
+<div class="row">
+    <div class="col-md-12 grid-margin">
+        <div class="row">
+            <div class="col-12 col-xl-8 mb-4 mb-xl-0">
+                <h3 class="font-weight-bold">Location Management</h3>
             </div>
         </div>
-
     </div>
+</div>
+<div class="row">
+  <div class="col-md-12 grid-margin stretch-card">
+    <div class="card">
+      <div class="card-body">
+        <h4 class="card-title">Edit Location</h4>
+        <form class="forms-sample" action="" method="post">
+        @csrf
+          <div class="form-group">
+            <label for="exampleInputUsername1">Location</label>
+            <input type="text" class="form-control" name="name" value="{{$location->name}}" required>
+          </div>
+          <button type="submit" class="btn btn-primary mr-2">Save</button>
+        </form>
+      </div>
+    </div>
+  </div>
 </div>
 
 @endsection
 
 @section('footerScripts')
-
-    <script>
-
-    </script>
-
 @endsection
